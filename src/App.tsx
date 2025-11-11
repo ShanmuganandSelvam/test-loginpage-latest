3ce39eeb8fa13efb2430ba0a15d9a47aaa4a3df6
Shanmuganand Selvam <shanmuganand.selvam.ext@bayer.com>
2025-11-11 11:27:29 +0530

    Initial project setup and files

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { LoginPage } from './components/login-page';
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gradient-to-br from-blue-50 to-indigo-100">
+      <LoginPage />
+    </div>
+  );
+}
+
+export default App;
